26e1b6c28452920b28987acc72f23594c5726aab
Alberto Banet <abanet@ucm.es>
2016-05-25 12:58:41 +0200

    Initial Commit

---
+++ ondafuenlabrada/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ondafuenlabrada
+//
+//  Created by Alberto Banet Masa on 25/5/16.
+//  Copyright © 2016 abanet. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
