8bfd5fe71a07b9d43e98f9c6bc4055cec441fecb
Alberto Banet <abanet@ucm.es>
2016-06-07 12:39:07 +0200

    Se añade el control de red

--- ondafuenlabrada/ViewController.swift
+++ ondafuenlabrada/ViewController.swift
@@ -21,25 +21,58 @@ class ViewController: UIViewController {
   @IBOutlet weak var btnPausa: UIButton!
   
   
-  let radioPlayer = Player.radio
+  let urlString = "http://radioserver3.profesionalhosting.com:8024/;stream.mp3"
+  var radioPlayer = AVPlayer(URL: NSURL(string: "http://radioserver3.profesionalhosting.com:8024/;stream.mp3")!)
+  var estaSonando = true
   
   // Apple: An audio session is a singleton object that you employ to set the audio context for your app and to express to the system your intentions for your app’s audio behavior.
   let session: AVAudioSession = AVAudioSession.sharedInstance()
 
   var mpVolumeSlider = UISlider()
+  
+  // Detección de red
+  var reachability: Reachability?
 
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
-        radioPlayer.addObserver(self, forKeyPath: "status", options: NSKeyValueObservingOptions(), context: nil)
-    
-  }
+      }
   
   
   override func viewWillAppear(animated: Bool) {
-    // Configuración de la radio
+    
     radioPlayer.rate = 1.0
+    radioPlayer.play()
+
+    // Detección de red
+    do {
+      reachability = try Reachability.reachabilityForInternetConnection()
+    } catch {
+      print("no se puede crear reachability")
+      return
+    }
+    
+    NSNotificationCenter.defaultCenter().addObserver(self, selector:#selector(self.reachabilityChanged(_:)), name: ReachabilityChangedNotification, object: reachability)
+    do {
+      try reachability?.startNotifier()
+    } catch {
+      print("no se ha podido iniciar el comienzo de las notificaciones")
+    }
+    
+    if reachability!.isReachable() {
+      print("viewVillAppear: hay red")
+      if !estaSonando {
+        print("Creando y lanzando radio")
+        //radioPlayer = AVPlayer(URL: NSURL(string: urlString)!)
+        //radioPlayer!.rate = 1.0
+        playRadio(btnPlay)
+      }
+    } else {
+      print("viewVillAppear: NO hay red")
+    }
+    
+    print("entrando en willAppear. Avplayer: \(radioPlayer)")
+    
     
     // Inicialmente la radio está encendida
     btnPlay.enabled = false
@@ -58,43 +91,46 @@ class ViewController: UIViewController {
     // Creamos y lanzamos la animación (al arrancar la app se escuchará la radio por defecto)
     createNowPlayingAnimation()
     startNowPlayingAnimation()
+    estaSonando = true
 
     
   }
+  
+  override func viewWillDisappear(animated: Bool) {
+    // Dejamos de escuchar para saber si tenemos red
+    reachability?.stopNotifier()
+    NSNotificationCenter.defaultCenter().removeObserver(self,
+                                                        name: ReachabilityChangedNotification,
+                                                        object: reachability)
+  }
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
   
-  deinit {
-    radioPlayer.removeObserver(self, forKeyPath: "status", context: nil)
-  }
 
-  // OBSERVADORES
-  override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
-    
-    if object as! NSObject == radioPlayer {
-        print("observador dice estado = \(radioPlayer.status.rawValue)")
-    }
-    
-    
-  }
+
 
   // MARK:  Botones de radio
   @IBAction func playRadio(sender: AnyObject) {
     print("Play pulsado: \(radioPlayer.status.rawValue)")
+    print("AVPlayer en el momento de pulsar Play: \(radioPlayer)")
     radioPlayer.play()
     startNowPlayingAnimation()
     btnPlay.enabled = false
     btnPausa.enabled = true
+    estaSonando = true
   }
   
   
   @IBAction func stopRadio(sender: AnyObject) {
     stopNowPlayingAnimation()
     radioPlayer.pause()
+    radioPlayer.cancelPendingPrerolls() // no parece tener efecto.
     btnPlay.enabled = true
     btnPausa.enabled = false
+    estaSonando = false
 
   }
   
@@ -146,6 +182,23 @@ class ViewController: UIViewController {
   // let activityViewController = UIActivityViewController(activityItems: [songToShare, track.artworkImage!], applicationActivities: nil)
     let activityViewController = UIActivityViewController(activityItems: [songToShare, logoOndoFuenlabrada.image!], applicationActivities: nil)
     presentViewController(activityViewController, animated: true, completion: nil)
+  }
+  
+  // MARK: reachabilityChanged
+  func reachabilityChanged(note: NSNotification) {
+    
+    let reachability = note.object as! Reachability
+    
+    if reachability.isReachable() {
+      if reachability.isReachableViaWiFi() {
+        print("Reachable via WiFi")
+      } else {
+        print("Reachable via Cellular")
+      }
+    } else {
+      print("Network not reachable")
+      stopRadio(btnPausa)
+    }
   }
   
 }
